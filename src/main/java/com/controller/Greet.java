0292000f371b031bcb0f731dfd8703e8f463c7bb
Ganesh Dhore <ganesh.dhore@austere.co.in>
2023-10-06 10:52:56 +0530

    changes in greet controller

--- src/main/java/com/controller/Greet.java
+++ src/main/java/com/controller/Greet.java
@@ -1,20 +1,20 @@
 package com.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Greet {
-
-	@Autowired
-	private Environment env;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.core.env.Environment;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.RestController;
 	
-	@GetMapping("/greet")
-	public String get() {
-		String port = env.getProperty("server.port");
-		return "Welcome to Service-2 :: GREET :: PORT Number :: "+port;
+	@RestController
+	public class Greet {
+	
+		@Autowired
+		private Environment env;
+		
+		@GetMapping("/greet")
+		public String get() {
+			String port = env.getProperty("server.port");
+			return "Welcome to Service-2 :: GREET :: PORT Number :: "+port;
+		}
+		
 	}
-	
-}
